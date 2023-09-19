883544bb4e212dd7f297228b4239833cf40d1428
DraggyChen <115056561+DraggyChen@users.noreply.github.com>
2023-09-19 08:46:34 +0800

    註冊畫面：性別欄位置左之前

--- lib/register/personal_data.dart
+++ lib/register/personal_data.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:pickup/style/styles.dart';
 
-
-class PersonalData extends StatefulWidget{
+class PersonalData extends StatefulWidget {
   @override
   _PersonalDataState createState() => _PersonalDataState();
 }
 
-class _PersonalDataState extends State<PersonalData>{
+class _PersonalDataState extends State<PersonalData> {
+  /* 姓名 */
   final TextEditingController _usernameController = TextEditingController();
 
   /* 手機號碼 and Formatter */
@@ -18,6 +19,9 @@ class _PersonalDataState extends State<PersonalData>{
     initialText: '+886-',
   );
 
+  /* 性別 */
+  String? _selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,36 +31,71 @@ class _PersonalDataState extends State<PersonalData>{
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             SizedBox(height: 20),
-            Text("基本資料"),
-
+            Text(
+              "基本資料",
+              style: CustomStyles.topTitleStyle,
+            ),
             TextField(
               controller: _usernameController,
               decoration: InputDecoration(
                 labelText: "姓名",
-                border: OutlineInputBorder(),
+                border: UnderlineInputBorder(),
               ),
+              keyboardType: TextInputType.name,
             ),
-
             SizedBox(height: 20),
 
             /* 手機號碼 */
             TextField(
               controller: _phoneController,
               decoration: InputDecoration(
-                labelText: "手機號碼",
-                border: OutlineInputBorder(),
-                hintText: '+886-###-###-###'
-              ),
+                  labelText: "手機號碼",
+                  border: UnderlineInputBorder(),
+                  hintText: '+886-###-###-###'),
               inputFormatters: [maskFormatter],
               keyboardType: TextInputType.phone,
             ),
+            SizedBox(height: 20),
 
-
-
+            /* 性別選擇 */
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text("性別"),
+            ),
+            Row(
+              children: [
+                Expanded(
+                    child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: ListTile(
+                      title: const Text("男"),
+                      leading: Radio<String>(
+                          value: '男',
+                          groupValue: _selectedGender,
+                          onChanged: (String? value) {
+                            setState(() {
+                              _selectedGender = value;
+                            });
+                          })),
+                )),
+                Expanded(
+                  child: ListTile(
+                      title: const Text("女"),
+                      leading: Radio<String>(
+                        value: "女",
+                        groupValue: _selectedGender,
+                        onChanged: (String? value) {
+                          setState(() {
+                            _selectedGender = value;
+                          });
+                        },
+                      )),
+                )
+              ],
+            ),
           ],
         ),
       ),
     );
   }
-  
 }
---
+++ lib/style/styles.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomStyles {
+  static const TextStyle topTitleStyle = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    fontFamily: 'fontFamily',
+  );
+
+  static const TextStyle leftContentStyle = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+}
