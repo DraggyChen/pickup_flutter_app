ccc19da3af53e209048852ea053de1b78486c03b
DraggyChen <115056561+DraggyChen@users.noreply.github.com>
2023-09-19 16:11:47 +0800

    註冊畫面：滾動式ScrollView、瀏海問題解決

--- lib/register/personal_data.dart
+++ lib/register/personal_data.dart
@@ -3,7 +3,6 @@ import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:pickup/style/styles.dart';
 import 'package:pickup/utils/ImagePickerWidget.dart';
 
-
 class PersonalData extends StatefulWidget {
   @override
   _PersonalDataState createState() => _PersonalDataState();
@@ -38,131 +37,145 @@ class _PersonalDataState extends State<PersonalData> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height: 20),
-            Text(
-              "基本資料",
-              style: CustomStyles.topTitleStyle,
-            ),
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                labelText: "姓名",
-                border: UnderlineInputBorder(),
-              ),
-              keyboardType: TextInputType.name,
-            ),
-            SizedBox(height: 20),
-
-            /* 手機號碼 */
-            TextField(
-              controller: _phoneController,
-              decoration: InputDecoration(
-                  labelText: "手機號碼",
-                  border: UnderlineInputBorder(),
-                  hintText: '+886-###-###-###'),
-              inputFormatters: [maskFormatter],
-              keyboardType: TextInputType.phone,
-            ),
-            SizedBox(height: 20),
-
-            /* 性別選擇 */
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text("性別"),
-            ),
-            Row(
+        //排版處理：上方針對瀏海部分的話再往下加
+          padding: EdgeInsets.only(
+            top: 16.0 + MediaQuery.of(context).padding.top,
+            left: 16.0,
+            right: 16.0,
+            bottom: 16.0,
+          ),
+          //元件內容
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Expanded(
-                    child: ListTile(
-                  title: const Text(men),
-                  leading: Radio<String>(
-                      value: men,
-                      groupValue: _selectedGender,
+                SizedBox(height: 20),
+                Text(
+                  "基本資料",
+                  style: CustomStyles.topTitleStyle,
+                ),
+                TextField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    labelText: "姓名",
+                    border: UnderlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.name,
+                ),
+                SizedBox(height: 20),
+
+                /* 手機號碼 */
+                TextField(
+                  controller: _phoneController,
+                  decoration: InputDecoration(
+                      labelText: "手機號碼",
+                      border: UnderlineInputBorder(),
+                      hintText: '+886-###-###-###'),
+                  inputFormatters: [maskFormatter],
+                  keyboardType: TextInputType.phone,
+                ),
+                SizedBox(height: 20),
+
+                /* 性別選擇 */
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("性別"),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                        child: ListTile(
+                      title: const Text(men),
+                      leading: Radio<String>(
+                          value: men,
+                          groupValue: _selectedGender,
+                          onChanged: (String? value) {
+                            setState(() {
+                              _selectedGender = value;
+                            });
+                          }),
+                    )),
+                    Expanded(
+                      child: ListTile(
+                          title: const Text(women),
+                          leading: Radio<String>(
+                            value: women,
+                            groupValue: _selectedGender,
+                            onChanged: (String? value) {
+                              setState(() {
+                                _selectedGender = value;
+                              });
+                            },
+                          )),
+                    )
+                  ],
+                ),
+
+                /* 學校選擇 */
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("學校"),
+                ),
+                Row(
+                  children: [
+                    Radio<String>(
+                      value: uni_CGU,
+                      groupValue: _selectedUniversity,
                       onChanged: (String? value) {
                         setState(() {
-                          _selectedGender = value;
+                          _selectedUniversity = value;
                         });
-                      }),
-                )),
-                Expanded(
-                  child: ListTile(
-                      title: const Text(women),
-                      leading: Radio<String>(
-                        value: women,
-                        groupValue: _selectedGender,
-                        onChanged: (String? value) {
-                          setState(() {
-                            _selectedGender = value;
-                          });
-                        },
-                      )),
-                )
+                      },
+                    ),
+                    Text(uni_CGU),
+                    Radio<String>(
+                      value: uni_CGUST,
+                      groupValue: _selectedUniversity,
+                      onChanged: (String? value) {
+                        setState(() {
+                          _selectedUniversity = value;
+                        });
+                      },
+                    ),
+                    Text(uni_CGUST),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Radio<String>(
+                      value: uni_NTSU,
+                      groupValue: _selectedUniversity,
+                      onChanged: (String? value) {
+                        setState(() {
+                          _selectedUniversity = value;
+                        });
+                      },
+                    ),
+                    Text(uni_NTSU),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+
+                /* 學生證或教職員工作證 */
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("學生證或教職員工作證"),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ImagePickerWidget(hint: '正面(大頭照)', width: 200, height: 100),
+                SizedBox(height: 20),
+                ImagePickerWidget(
+                  hint: '反面(學校名稱)',
+                  width: 200,
+                  height: 100,
+                ),
               ],
             ),
-
-            /* 學校選擇 */
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text("學校"),
-            ),
-            Row(
-              children: [
-                Radio<String>(
-                  value: uni_CGU,
-                  groupValue: _selectedUniversity,
-                  onChanged: (String? value) {
-                    setState(() {
-                      _selectedUniversity = value;
-                    });
-                  },
-                ),
-                Text(uni_CGU),
-                Radio<String>(
-                  value: uni_CGUST,
-                  groupValue: _selectedUniversity,
-                  onChanged: (String? value) {
-                    setState(() {
-                      _selectedUniversity = value;
-                    });
-                  },
-                ),
-                Text(uni_CGUST),
-              ],
-            ),
-            Row(
-              children: [
-                Radio<String>(
-                  value: uni_NTSU,
-                  groupValue: _selectedUniversity,
-                  onChanged: (String? value) {
-                    setState(() {
-                      _selectedUniversity = value;
-                    });
-                  },
-                ),
-                Text(uni_NTSU),
-              ],
-            ),
-            SizedBox(height: 20,),
-
-            /* 學生證或教職員工作證 */
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text("學生證或教職員工作證"),
-            ),
-            SizedBox(height: 20,),
-            ImagePickerWidget(hint: '正面(大頭照)', width: 200, height: 100),
-            SizedBox(height: 20),
-            ImagePickerWidget(hint: '反面(學校名稱)', width: 200, height: 100,),
-
-
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
